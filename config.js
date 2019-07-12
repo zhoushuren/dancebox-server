589535225c74773df9461270ac703a71545297d2
jim <zhoujun247@foxmail.com>
2019-07-13 00:20:17 +0800

    config RDS

--- config.js
+++ config.js
@@ -2,9 +2,10 @@ const Sequelize = require('sequelize')
 const DB_NAME =  process.env.DB_NAME || 'box'
 const DB_USER =  process.env.DB_USER || 'root'
 const DB_PASS =  process.env.DB_PASS || ''
+const DB_HOST =  process.env.DB_HOST || 'localhost'
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-    host: 'localhost',
+    host: DB_HOST,
     dialect: 'mysql',
     pool: {
         max: 500,
